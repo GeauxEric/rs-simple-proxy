8e033eb5c68d0c669a8011a8d521bc42f6e85c82
Yun Ding <yunding.eric@gmail.com>
2022-03-06 15:35:37 -0800

    [rate-limit] set up integration test

---
+++ tests/integration_test.rs
@@ -0,0 +1,73 @@
+use std::path::PathBuf;
+
+use hyper::client::Client;
+use log::info;
+
+use simple_proxy::Environment;
+use simple_proxy::middlewares::Logger;
+use simple_proxy::middlewares::Router;
+use simple_proxy::middlewares::router::RouterConfig;
+use simple_proxy::SimpleProxy;
+
+mod common;
+
+#[cfg(test)]
+mod tests {
+    use std::error::Error;
+
+    use tokio::task::JoinHandle;
+    use tokio::time::Duration;
+
+    use super::*;
+
+    const TEST_PORT: u16 = 3456;
+
+    #[derive(Debug, Clone)]
+    pub struct Config(String);
+
+    impl RouterConfig for Config {
+        fn get_router_filename(&self) -> &str {
+            self.0.as_str()
+        }
+    }
+
+    fn build_server() -> SimpleProxy {
+        let mut proxy = SimpleProxy::new(TEST_PORT, Environment::Development);
+        let logger = Logger::new();
+
+        let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        d.push("tests/test_routes.json");
+        let path = d.to_str().unwrap().to_string();
+        let router = Router::new(&Config(path));
+
+        // Order matters
+        proxy.add_middleware(Box::new(router));
+        proxy.add_middleware(Box::new(logger));
+
+        return proxy;
+    }
+
+    async fn start_server() -> JoinHandle<Result<(), Box<dyn Error + Send + Sync>>> {
+        let server = build_server();
+
+        info!("Starting proxy server in the background");
+        tokio::spawn(async move { server.run().await })
+    }
+
+    #[tokio::test]
+    async fn proxy_works() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+        let _ = pretty_env_logger::try_init();
+
+        start_server().await;
+
+        tokio::time::sleep(Duration::from_secs(1)).await;
+
+        let client = Client::new();
+
+        let uri = format!("http://localhost:{}/test_from", TEST_PORT).parse()?;
+        let resp = client.get(uri).await?;
+        assert_eq!(404, resp.status().as_u16());
+
+        Ok(())
+    }
+}
